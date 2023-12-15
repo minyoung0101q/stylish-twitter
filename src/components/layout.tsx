b9fdf3d9a78e2708d3d2632ce1a3d23aa9118a64
stylish_code <minyoung0101q@gmail.com>
2023-12-15 21:35:32 +0900

    Navigation Bar

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,9 +1,112 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
+import { styled } from "styled-components";
+import { auth } from "../firebase";
+
+const Wrapper = styled.div`
+  display: grid;
+  gap: 20px;
+  grid-template-columns: 1fr 4fr;
+  height: 100%;
+  padding: 50px 0px;
+  width: 100%;
+  max-width: 860px;
+`;
+
+const Menu = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+`;
+
+const MenuItem = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 2px solid white;
+  height: 50px;
+  width: 50px;
+  border-radius: 50%;
+  svg {
+    width: 30px;
+    fill: white;
+  }
+  &.log-out {
+    border-color: tomato;
+    svg {
+      fill: tomato;
+    }
+  }
+`;
 
 export default function Layout() {
+  const navigate = useNavigate();
+  const onLogOut = async () => {
+    // 확인 버튼을 누르면 ok 변수 값이 true가 될 것
+    const ok = confirm("Are you sure you wnat to logout?");
+    // console.log(ok);
+    // 만약에 ok가 true라면 auth.signOut 함수를 호출한다.
+    if (ok) {
+      await auth.signOut();
+      // 위에서 결과값을 반환하면useNavigate 훅을 사용해서
+      // login 페이지로 돌아가게 한다.
+      navigate("/login");
+    }
+  };
   return (
-    <>
+    <Wrapper>
+      <Menu>
+        <Link to="/">
+          <MenuItem>
+            <svg
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path
+                clipRule="evenodd"
+                fillRule="evenodd"
+                d="M9.293 2.293a1 1 0 011.414 0l7 7A1 1 0 0117 11h-1v6a1 1 0 01-1 1h-2a1 1 0 01-1-1v-3a1 1 0 00-1-1H9a1 1 0 00-1 1v3a1 1 0 01-1 1H5a1 1 0 01-1-1v-6H3a1 1 0 01-.707-1.707l7-7z"
+              />
+            </svg>
+          </MenuItem>
+        </Link>
+        <Link to="/profile">
+          <MenuItem>
+            <svg
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+            >
+              <path d="M7 8a3 3 0 100-6 3 3 0 000 6zM14.5 9a2.5 2.5 0 100-5 2.5 2.5 0 000 5zM1.615 16.428a1.224 1.224 0 01-.569-1.175 6.002 6.002 0 0111.908 0c.058.467-.172.92-.57 1.174A9.953 9.953 0 017 18a9.953 9.953 0 01-5.385-1.572zM14.5 16h-.106c.07-.297.088-.611.048-.933a7.47 7.47 0 00-1.588-3.755 4.502 4.502 0 015.874 2.636.818.818 0 01-.36.98A7.465 7.465 0 0114.5 16z" />
+            </svg>
+          </MenuItem>
+        </Link>
+        {/* user logout이라 Link 없음 */}
+        <MenuItem onClick={onLogOut} className="log-out">
+          <svg
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path
+              clipRule="evenodd"
+              fillRule="evenodd"
+              d="M3 4.25A2.25 2.25 0 015.25 2h5.5A2.25 2.25 0 0113 4.25v2a.75.75 0 01-1.5 0v-2a.75.75 0 00-.75-.75h-5.5a.75.75 0 00-.75.75v11.5c0 .414.336.75.75.75h5.5a.75.75 0 00.75-.75v-2a.75.75 0 011.5 0v2A2.25 2.25 0 0110.75 18h-5.5A2.25 2.25 0 013 15.75V4.25z"
+            />
+            <path
+              clipRule="evenodd"
+              fillRule="evenodd"
+              d="M19 10a.75.75 0 00-.75-.75H8.704l1.048-.943a.75.75 0 10-1.004-1.114l-2.5 2.25a.75.75 0 000 1.114l2.5 2.25a.75.75 0 101.004-1.114l-1.048-.943h9.546A.75.75 0 0019 10z"
+            />
+          </svg>
+        </MenuItem>
+      </Menu>
       <Outlet />
-    </>
+    </Wrapper>
   );
 }
